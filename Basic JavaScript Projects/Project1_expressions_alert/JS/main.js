87d99fb9a680bad9942e9bfe5e0cf48027fef6f8
tysonlatek <119722869+tysonlatek@users.noreply.github.com>
2023-04-01 01:19:32 -0400

    Practiced basic JavaScript
    Practiced assigning variables, using methods, writing strings, comments, and expressions, and printing outputs.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+window.alert("Hello, World!") // Displays a pop up window saying "Hello, World!"
+var Q = "\"How are you doing today?\" he asked.", A = " \"I\'m doing great!\" she responded."; //Assigns a variable named Q a string of text
+document.write(Q + A) //Prints variables Q's and A's strings on the screen
+var Math = 3 + 2 //Example of an expression
+document.write(" " + Math)
